0dbccf8f05da204a5b1f21113abfdfda5925d2d9
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-24 10:21:03 +0200

    FET: Domain delivery server skeleton

--- api/domain_delivery_servers.go
+++ api/domain_delivery_servers.go
@@ -7,3 +7,38 @@
 
 // Package api Golang bindings for Baruwa REST API
 package api
+
+// DomainDeliveryServer holds domain delivery servers
+type DomainDeliveryServer struct {
+	ID               int    `json:"id,omitempty"`
+	Address          string `json:"address"`
+	Protocol         int    `json:"protocol"`
+	Port             int    `json:"port"`
+	RequireTLS       bool   `json:"require_tls"`
+	VerificationOnly bool   `json:"verification_only"`
+	Enabled          bool   `json:"enabled"`
+}
+
+// GetDomainDeliveryServer returns a domain delivery server
+// https://www.baruwa.com/docs/api/#retrieve-a-delivery-server
+func (c *Client) GetDomainDeliveryServer(id int) (domain *DomainDeliveryServer, err error) {
+	return
+}
+
+// CreateDomainDeliveryServer creates a domain delivery server
+// https://www.baruwa.com/docs/api/#create-a-delivery-server
+func (c *Client) CreateDomainDeliveryServer(domain *DomainDeliveryServer) (err error) {
+	return
+}
+
+// UpdateDomainDeliveryServer updates a domain delivery server
+// https://www.baruwa.com/docs/api/#update-a-delivery-server
+func (c *Client) UpdateDomainDeliveryServer(domain *DomainDeliveryServer) (err error) {
+	return
+}
+
+// DeleteDomainDeliveryServer deletes a domain delivery server
+// https://www.baruwa.com/docs/api/#delete-a-delivery-server
+func (c *Client) DeleteDomainDeliveryServer(id int) (err error) {
+	return
+}
