d389a496ca1381afcb13bc4449b6a2d47dbbe929
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-24 09:38:04 +0200

    FET: Implement alias address skeleton

--- api/user_aliases.go
+++ api/user_aliases.go
@@ -7,3 +7,92 @@
 
 // Package api Golang bindings for Baruwa REST API
 package api
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/google/go-querystring/query"
+)
+
+// Alias hosts alias addresses
+// https://www.baruwa.com/docs/api/#alias-addresses
+type Alias struct {
+	ID      int    `json:"id,omitempty"`
+	Address string `json:"address"`
+	Enabled bool   `json:"enabled"`
+}
+
+// GetAlias returns an alias address
+// https://www.baruwa.com/docs/api/#retrieve-an-existing-alias-address
+func (c *Client) GetAlias(id int) (alias *Alias, err error) {
+	if id <= 0 {
+		err = fmt.Errorf("The id param should be > 0")
+		return
+	}
+
+	err = c.get(fmt.Sprintf("aliasaddresses/%d", id), alias)
+
+	return
+}
+
+// CreateAlias creates an alias address
+// https://www.baruwa.com/docs/api/#create-an-alias-address
+func (c *Client) CreateAlias(userid int, alias *Alias) (err error) {
+	var v url.Values
+
+	if userid <= 0 {
+		err = fmt.Errorf("The userid param should be > 0")
+		return
+	}
+
+	if alias == nil {
+		err = fmt.Errorf("The alias param cannot be nil")
+		return
+	}
+
+	if v, err = query.Values(alias); err != nil {
+		return
+	}
+
+	err = c.post(fmt.Sprintf("aliasaddresses/%d", userid), v, alias)
+
+	return
+}
+
+// UpdateAlias updates an alias address
+// https://www.baruwa.com/docs/api/#update-an-alias-address
+func (c *Client) UpdateAlias(alias *Alias) (err error) {
+	var v url.Values
+
+	if alias == nil {
+		err = fmt.Errorf("The alias param cannot be nil")
+		return
+	}
+
+	if alias.ID <= 0 {
+		err = fmt.Errorf("The alias.ID param should be > 0")
+		return
+	}
+
+	if v, err = query.Values(alias); err != nil {
+		return
+	}
+
+	err = c.put(fmt.Sprintf("aliasaddresses/%d", alias.ID), v, alias)
+
+	return
+}
+
+// DeleteAlias deletes an alias address
+// https://www.baruwa.com/docs/api/#delete-an-alias-address
+func (c *Client) DeleteAlias(id int) (err error) {
+	if id <= 0 {
+		err = fmt.Errorf("The id param should be > 0")
+		return
+	}
+
+	err = c.delete(fmt.Sprintf("aliasaddresses/%d", id))
+
+	return
+}
