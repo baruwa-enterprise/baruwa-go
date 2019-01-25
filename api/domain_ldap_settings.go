b2855197f6750d6e48d0c17742e7fcd805af81c2
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-25 09:00:19 +0200

    FET: Domain LDAP settings skeleton

--- api/domain_ldap_settings.go
+++ api/domain_ldap_settings.go
@@ -7,3 +7,43 @@
 
 // Package api Golang bindings for Baruwa REST API
 package api
+
+// LDAPSettings holds Domain LDAP settings
+type LDAPSettings struct {
+	ID                int    `json:"id,omitempty"`
+	Basedn            string `json:"basedn"`
+	NameAttribute     string `json:"nameattribute"`
+	EmailAttribute    string `json:"emailattribute"`
+	BindDN            string `json:"binddn"`
+	BindPw            string `json:"bindpw"`
+	UseTLS            bool   `json:"usetls"`
+	UseSearch         bool   `json:"usesearch"`
+	SearchFilter      string `json:"searchfilter"`
+	SearchScope       string `json:"search_scope"`
+	EmailSearchFilter string `json:"emailsearchfilter"`
+	EmailSearchDcope  string `json:"emailsearch_scope"`
+}
+
+// GetLDAPSettings returns a domain smarthost
+// https://www.baruwa.com/docs/api/#retrieve-ad-ldap-settings
+func (c *Client) GetLDAPSettings(id int) (server *LDAPSettings, err error) {
+	return
+}
+
+// CreateLDAPSettings creates a domain smarthost
+// https://www.baruwa.com/docs/api/#create-ad-ldap-settings
+func (c *Client) CreateLDAPSettings(server *LDAPSettings) (err error) {
+	return
+}
+
+// UpdateLDAPSettings updates a domain smarthost
+// https://www.baruwa.com/docs/api/#update-ad-ldap-settings
+func (c *Client) UpdateLDAPSettings(server *LDAPSettings) (err error) {
+	return
+}
+
+// DeleteLDAPSettings deletes a domain smarthost
+// https://www.baruwa.com/docs/api/#delete-ad-ldap-settings
+func (c *Client) DeleteLDAPSettings(id int) (err error) {
+	return
+}
