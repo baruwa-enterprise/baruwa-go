64621861b0b799a172bed9d4c5381d3ffbd27051
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-25 08:51:09 +0200

    FET: Organization smarthost skeleton

--- api/organization_smarthosts.go
+++ api/organization_smarthosts.go
@@ -7,3 +7,39 @@
 
 // Package api Golang bindings for Baruwa REST API
 package api
+
+// OrgSmartHost holds domain smarthosts
+type OrgSmartHost struct {
+	ID          int    `json:"id,omitempty"`
+	Address     string `json:"address"`
+	Username    string `json:"username"`
+	Password    string `json:"password,omitempty"`
+	Port        int    `json:"port"`
+	RequireTLS  bool   `json:"require_tls"`
+	Enabled     bool   `json:"enabled"`
+	Description string `json:"description"`
+}
+
+// GetOrgSmartHost returns a domain smarthost
+// https://www.baruwa.com/docs/api/#retrieve-a-organization-smarthost
+func (c *Client) GetOrgSmartHost(id int) (server *OrgSmartHost, err error) {
+	return
+}
+
+// CreateOrgSmartHost creates a domain smarthost
+// https://www.baruwa.com/docs/api/#create-an-organization-smarthost
+func (c *Client) CreateOrgSmartHost(server *OrgSmartHost) (err error) {
+	return
+}
+
+// UpdateOrgSmartHost updates a domain smarthost
+// https://www.baruwa.com/docs/api/#update-a-organization-smarthost
+func (c *Client) UpdateOrgSmartHost(server *OrgSmartHost) (err error) {
+	return
+}
+
+// DeleteOrgSmartHost deletes a domain smarthost
+// https://www.baruwa.com/docs/api/#retrieve-system-status
+func (c *Client) DeleteOrgSmartHost(id int) (err error) {
+	return
+}
