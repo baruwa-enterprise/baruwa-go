7e3fb4770895f4f1a555f470e007b0215bc93ce8
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-25 08:40:19 +0200

    FET: Authentication settings skeleton

--- api/domain_authentication_settings.go
+++ api/domain_authentication_settings.go
@@ -7,3 +7,38 @@
 
 // Package api Golang bindings for Baruwa REST API
 package api
+
+// AuthServer holds an authentication server
+type AuthServer struct {
+	ID              int    `json:"id,omitempty"`
+	Address         string `json:"address"`
+	Protocol        int    `json:"protocol"`
+	Port            int    `json:"port"`
+	Enabled         bool   `json:"enabled"`
+	SplitAddress    bool   `json:"split_address"`
+	UserMapTemplate string `json:"user_map_template"`
+}
+
+// GetAuthServer returns an authentication server
+// https://www.baruwa.com/docs/api/#retrieve-authentication-settings
+func (c *Client) GetAuthServer(id int) (server AuthServer, err error) {
+	return
+}
+
+// CreateAuthServer creates an authentication server
+// https://www.baruwa.com/docs/api/#create-authentication-settings
+func (c *Client) CreateAuthServer(server *AuthServer) (err error) {
+	return
+}
+
+// UpdateAuthServer updates an authentication server
+// https://www.baruwa.com/docs/api/#update-authentication-settings
+func (c *Client) UpdateAuthServer(server *AuthServer) (err error) {
+	return
+}
+
+// DeleteAuthServer deletes an authentication server
+// https://www.baruwa.com/docs/api/#delete-authentication-settings
+func (c *Client) DeleteAuthServer(id int) (err error) {
+	return
+}
