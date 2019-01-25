94f0448a006fc0e36a8472e2875399551bbb2ad2
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-25 08:50:54 +0200

    FET: Domain smarthost skeleton

--- api/domain_smarthosts.go
+++ api/domain_smarthosts.go
@@ -7,3 +7,39 @@
 
 // Package api Golang bindings for Baruwa REST API
 package api
+
+// DomainSmartHost holds domain smarthosts
+type DomainSmartHost struct {
+	ID          int    `json:"id,omitempty"`
+	Address     string `json:"address"`
+	Username    string `json:"username"`
+	Password    string `json:"password,omitempty"`
+	Port        int    `json:"port"`
+	RequireTLS  bool   `json:"require_tls"`
+	Enabled     bool   `json:"enabled"`
+	Description string `json:"description"`
+}
+
+// GetDomainSmartHost returns a domain smarthost
+// https://www.baruwa.com/docs/api/#retrieve-a-domain-smarthost
+func (c *Client) GetDomainSmartHost(id int) (server *DomainSmartHost, err error) {
+	return
+}
+
+// CreateDomainSmartHost creates a domain smarthost
+// https://www.baruwa.com/docs/api/#create-a-domain-smarthost
+func (c *Client) CreateDomainSmartHost(server *DomainSmartHost) (err error) {
+	return
+}
+
+// UpdateDomainSmartHost updates a domain smarthost
+// https://www.baruwa.com/docs/api/#update-a-domain-smarthost
+func (c *Client) UpdateDomainSmartHost(server *DomainSmartHost) (err error) {
+	return
+}
+
+// DeleteDomainSmartHost deletes a domain smarthost
+// https://www.baruwa.com/docs/api/#delete-a-domain-smarthost
+func (c *Client) DeleteDomainSmartHost(id int) (err error) {
+	return
+}
