96b7579be2a961c2a9f36386ed5dc33afdf75cd7
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-25 09:41:46 +0200

    FET: Organization skeleton

--- api/organization.go
+++ api/organization.go
@@ -7,3 +7,48 @@
 
 // Package api Golang bindings for Baruwa REST API
 package api
+
+// OrgDomain hold alias domain entries
+type OrgDomain struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+// Organization holds organizations
+type Organization struct {
+	ID      int         `json:"id,omitempty"`
+	Name    string      `json:"name"`
+	Domains []OrgDomain `json:"domains,omitempty"`
+}
+
+// OrganizationForm used for creation and update of organizations
+type OrganizationForm struct {
+	ID      int    `json:"id,omitempty"`
+	Name    string `json:"name"`
+	Domains []int  `json:"domains,omitempty"`
+	Admins  []int  `json:"admins,omitempty"`
+}
+
+// GetOrganization returns an organization
+// https://www.baruwa.com/docs/api/#retrieve-an-existing-organization
+func (c *Client) GetOrganization(id int) (org *Organization, err error) {
+	return
+}
+
+// CreateOrganization creates an organization
+// https://www.baruwa.com/docs/api/#create-an-organization
+func (c *Client) CreateOrganization(form *OrganizationForm, org Organization) (err error) {
+	return
+}
+
+// UpdateOrganization updates an organization
+// https://www.baruwa.com/docs/api/#update-an-organization
+func (c *Client) UpdateOrganization(form *OrganizationForm, org Organization) (err error) {
+	return
+}
+
+// DeleteOrganization deletes an organization
+// https://www.baruwa.com/docs/api/#delete-an-organization
+func (c *Client) DeleteOrganization(id int) (err error) {
+	return
+}
