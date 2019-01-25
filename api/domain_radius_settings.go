fa04ff4d6e4704150bd198d4c7ff170332163024
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-25 09:04:47 +0200

    FET: Domain Radius settings skeleton

--- api/domain_radius_settings.go
+++ api/domain_radius_settings.go
@@ -7,3 +7,34 @@
 
 // Package api Golang bindings for Baruwa REST API
 package api
+
+// RadiusSettings holds domain radius settings
+type RadiusSettings struct {
+	ID      int    `json:"id,omitempty"`
+	Secret  string `json:"secret"`
+	Timeout int    `json:"timeout"`
+}
+
+// GetRadiusSettings returns radius settings
+// https://www.baruwa.com/docs/api/#retrieve-radius-settings
+func (c *Client) GetRadiusSettings(id int) (server *RadiusSettings, err error) {
+	return
+}
+
+// CreateRadiusSettings creates radius settings
+// https://www.baruwa.com/docs/api/#create-radius-settings
+func (c *Client) CreateRadiusSettings(server *RadiusSettings) (err error) {
+	return
+}
+
+// UpdateRadiusSettings updates radius settings
+// https://www.baruwa.com/docs/api/#update-radius-settings
+func (c *Client) UpdateRadiusSettings(server *RadiusSettings) (err error) {
+	return
+}
+
+// DeleteRadiusSettings deletes radius settings
+// https://www.baruwa.com/docs/api/#delete-radius-settings
+func (c *Client) DeleteRadiusSettings(id int) (err error) {
+	return
+}
