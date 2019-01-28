f39cf1d52eb63b7d5ba45fd3abafe70783b63706
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-28 12:05:36 +0200

    FET: Domain Delivery servers implementation

--- api/domain_delivery_servers.go
+++ api/domain_delivery_servers.go
@@ -8,6 +8,13 @@
 // Package api Golang bindings for Baruwa REST API
 package api
 
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/google/go-querystring/query"
+)
+
 // DomainDeliveryServer holds domain delivery servers
 type DomainDeliveryServer struct {
 	ID               int    `json:"id,omitempty"`
@@ -21,24 +28,100 @@ type DomainDeliveryServer struct {
 
 // GetDomainDeliveryServer returns a domain delivery server
 // https://www.baruwa.com/docs/api/#retrieve-a-delivery-server
-func (c *Client) GetDomainDeliveryServer(id int) (server *DomainDeliveryServer, err error) {
+func (c *Client) GetDomainDeliveryServer(domainid, serverid int) (server *DomainDeliveryServer, err error) {
+	if domainid <= 0 {
+		err = fmt.Errorf("The domainid param should be > 0")
+		return
+	}
+
+	if serverid <= 0 {
+		err = fmt.Errorf("The serverid param should be > 0")
+		return
+	}
+
+	err = c.get(fmt.Sprintf("deliveryservers/%d/%d", domainid, serverid), server)
+
 	return
 }
 
 // CreateDomainDeliveryServer creates a domain delivery server
 // https://www.baruwa.com/docs/api/#create-a-delivery-server
-func (c *Client) CreateDomainDeliveryServer(server *DomainDeliveryServer) (err error) {
+func (c *Client) CreateDomainDeliveryServer(domainid int, server *DomainDeliveryServer) (err error) {
+	var v url.Values
+
+	if domainid <= 0 {
+		err = fmt.Errorf("The domainid param should be > 0")
+		return
+	}
+
+	if server == nil {
+		err = fmt.Errorf("The server param cannot be nil")
+		return
+	}
+
+	if v, err = query.Values(server); err != nil {
+		return
+	}
+
+	err = c.post(fmt.Sprintf("deliveryservers/%d", domainid), v, server)
+
 	return
 }
 
 // UpdateDomainDeliveryServer updates a domain delivery server
 // https://www.baruwa.com/docs/api/#update-a-delivery-server
-func (c *Client) UpdateDomainDeliveryServer(server *DomainDeliveryServer) (err error) {
+func (c *Client) UpdateDomainDeliveryServer(domainid int, server *DomainDeliveryServer) (err error) {
+	var v url.Values
+
+	if domainid <= 0 {
+		err = fmt.Errorf("The domainid param should be > 0")
+		return
+	}
+
+	if server == nil {
+		err = fmt.Errorf("The server param cannot be nil")
+		return
+	}
+
+	if server.ID <= 0 {
+		err = fmt.Errorf("The server.ID param should be > 0")
+		return
+	}
+
+	if v, err = query.Values(server); err != nil {
+		return
+	}
+
+	err = c.post(fmt.Sprintf("deliveryservers/%d/%d", domainid, server.ID), v, server)
+
 	return
 }
 
 // DeleteDomainDeliveryServer deletes a domain delivery server
 // https://www.baruwa.com/docs/api/#delete-a-delivery-server
-func (c *Client) DeleteDomainDeliveryServer(id int) (err error) {
+func (c *Client) DeleteDomainDeliveryServer(domainid int, server *DomainDeliveryServer) (err error) {
+	var v url.Values
+
+	if domainid <= 0 {
+		err = fmt.Errorf("The domainid param should be > 0")
+		return
+	}
+
+	if server == nil {
+		err = fmt.Errorf("The server param cannot be nil")
+		return
+	}
+
+	if server.ID <= 0 {
+		err = fmt.Errorf("The server.ID param should be > 0")
+		return
+	}
+
+	if v, err = query.Values(server); err != nil {
+		return
+	}
+
+	err = c.delete(fmt.Sprintf("deliveryservers/%d/%d", domainid, server.ID), v)
+
 	return
 }
