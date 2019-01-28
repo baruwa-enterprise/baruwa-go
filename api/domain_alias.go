0624f9982580c4faac07f565f814a3693a94b438
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-28 11:25:46 +0200

    FET: Domain Alias implementation

--- api/domain_alias.go
+++ api/domain_alias.go
@@ -8,6 +8,13 @@
 // Package api Golang bindings for Baruwa REST API
 package api
 
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/google/go-querystring/query"
+)
+
 // AliasDomain hold alias domain entries
 type AliasDomain struct {
 	ID   int    `json:"id"`
@@ -24,24 +31,89 @@ type DomainAlias struct {
 
 // GetDomainAlias returns a domain alias
 // https://www.baruwa.com/docs/api/#retrieve-domain-alias
-func (c *Client) GetDomainAlias(id int) (alias *DomainAlias, err error) {
+func (c *Client) GetDomainAlias(domainid, aliasid int) (alias *DomainAlias, err error) {
+	if domainid <= 0 {
+		err = fmt.Errorf("The domainid param should be > 0")
+		return
+	}
+
+	if aliasid <= 0 {
+		err = fmt.Errorf("The aliasid param should be > 0")
+		return
+	}
+
+	err = c.get(fmt.Sprintf("domainaliases/%d/%d", domainid, aliasid), alias)
+
 	return
 }
 
 // CreateDomainAlias creates a domain alias
 // https://www.baruwa.com/docs/api/#create-a-domain-alias
-func (c *Client) CreateDomainAlias(domain *DomainAlias) (err error) {
+func (c *Client) CreateDomainAlias(domainid int, alias *DomainAlias) (err error) {
+	var v url.Values
+
+	if domainid <= 0 {
+		err = fmt.Errorf("The domainid param should be > 0")
+		return
+	}
+
+	if alias == nil {
+		err = fmt.Errorf("The alias param cannot be nil")
+		return
+	}
+
+	if v, err = query.Values(alias); err != nil {
+		return
+	}
+
+	err = c.post(fmt.Sprintf("domainaliases/%d", domainid), v, alias)
+
 	return
 }
 
 // UpdateDomainAlias updates a domain alias
 // https://www.baruwa.com/docs/api/#update-a-domain-alias
-func (c *Client) UpdateDomainAlias(domain *DomainAlias) (err error) {
+func (c *Client) UpdateDomainAlias(domainid int, alias *DomainAlias) (err error) {
+	var v url.Values
+
+	if domainid <= 0 {
+		err = fmt.Errorf("The domainid param should be > 0")
+		return
+	}
+
+	if alias == nil {
+		err = fmt.Errorf("The alias param cannot be nil")
+		return
+	}
+
+	if alias.ID <= 0 {
+		err = fmt.Errorf("The alias.ID param should be > 0")
+		return
+	}
+
+	if v, err = query.Values(alias); err != nil {
+		return
+	}
+
+	err = c.put(fmt.Sprintf("domainaliases/%d/%d", domainid, alias.ID), v, alias)
+
 	return
 }
 
 // DeleteDomainAlias deletes an domain alias
 // https://www.baruwa.com/docs/api/#delete-a-domain-alias
-func (c *Client) DeleteDomainAlias(id int) (err error) {
+func (c *Client) DeleteDomainAlias(domainid, aliasid int) (err error) {
+	if domainid <= 0 {
+		err = fmt.Errorf("The domainid param should be > 0")
+		return
+	}
+
+	if aliasid <= 0 {
+		err = fmt.Errorf("The aliasid param should be > 0")
+		return
+	}
+
+	err = c.delete(fmt.Sprintf("domainaliases/%d/%d", domainid, aliasid))
+
 	return
 }
