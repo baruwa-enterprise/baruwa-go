47ac003f946393a53deb646071201081efb47c97
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-29 11:13:37 +0200

    FET: Fallback server implementation

--- api/organization_fallback_servers.go
+++ api/organization_fallback_servers.go
@@ -8,6 +8,13 @@
 // Package api Golang bindings for Baruwa REST API
 package api
 
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/google/go-querystring/query"
+)
+
 // FallBackServerOrg holds fallback server organization
 type FallBackServerOrg struct {
 	ID   int    `json:"id"`
@@ -27,24 +34,85 @@ type FallBackServer struct {
 
 // GetFallBackServer returns radius settings
 // https://www.baruwa.com/docs/api/#retrieve-a-fallback-server
-func (c *Client) GetFallBackServer(id int) (server *FallBackServer, err error) {
+func (c *Client) GetFallBackServer(serverID int) (server *FallBackServer, err error) {
+	if serverID <= 0 {
+		err = fmt.Errorf("The serverID param should be > 0")
+		return
+	}
+
+	err = c.get(fmt.Sprintf("failbackservers/%d", serverID), server)
+
 	return
 }
 
 // CreateFallBackServer creates radius settings
 // https://www.baruwa.com/docs/api/#create-a-fallback-server
-func (c *Client) CreateFallBackServer(server *FallBackServer) (err error) {
+func (c *Client) CreateFallBackServer(organizationID int, server *FallBackServer) (err error) {
+	var v url.Values
+
+	if organizationID <= 0 {
+		err = fmt.Errorf("The organizationID param should be > 0")
+		return
+	}
+
+	if server == nil {
+		err = fmt.Errorf("The server param cannot be nil")
+		return
+	}
+
+	if v, err = query.Values(server); err != nil {
+		return
+	}
+
+	err = c.post(fmt.Sprintf("failbackservers/%d", organizationID), v, server)
+
 	return
 }
 
 // UpdateFallBackServer updates radius settings
 // https://www.baruwa.com/docs/api/#update-a-fallback-server
 func (c *Client) UpdateFallBackServer(server *FallBackServer) (err error) {
+	var v url.Values
+
+	if server == nil {
+		err = fmt.Errorf("The server param cannot be nil")
+		return
+	}
+
+	if server.ID <= 0 {
+		err = fmt.Errorf("The server.ID param should be > 0")
+		return
+	}
+
+	if v, err = query.Values(server); err != nil {
+		return
+	}
+
+	err = c.put(fmt.Sprintf("failbackservers/%d", server.ID), v, server)
+
 	return
 }
 
 // DeleteFallBackServer deletes radius settings
 // https://www.baruwa.com/docs/api/#delete-a-fallback-server
-func (c *Client) DeleteFallBackServer(id int) (err error) {
+func (c *Client) DeleteFallBackServer(server *FallBackServer) (err error) {
+	var v url.Values
+
+	if server == nil {
+		err = fmt.Errorf("The server param cannot be nil")
+		return
+	}
+
+	if server.ID <= 0 {
+		err = fmt.Errorf("The server.ID param should be > 0")
+		return
+	}
+
+	if v, err = query.Values(server); err != nil {
+		return
+	}
+
+	err = c.delete(fmt.Sprintf("failbackservers/%d", server.ID), v)
+
 	return
 }
