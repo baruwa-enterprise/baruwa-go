67e87b94831ab8f65e67a615fbbcf548293a3814
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-29 10:55:06 +0200

    FET: Domain smarthost implementation

--- api/domain_smarthosts.go
+++ api/domain_smarthosts.go
@@ -8,6 +8,13 @@
 // Package api Golang bindings for Baruwa REST API
 package api
 
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/google/go-querystring/query"
+)
+
 // DomainSmartHost holds domain smarthosts
 type DomainSmartHost struct {
 	ID          int    `json:"id,omitempty"`
@@ -22,24 +29,100 @@ type DomainSmartHost struct {
 
 // GetDomainSmartHost returns a domain smarthost
 // https://www.baruwa.com/docs/api/#retrieve-a-domain-smarthost
-func (c *Client) GetDomainSmartHost(id int) (server *DomainSmartHost, err error) {
+func (c *Client) GetDomainSmartHost(domainid, serverid int) (server *DomainSmartHost, err error) {
+	if domainid <= 0 {
+		err = fmt.Errorf("The domainid param should be > 0")
+		return
+	}
+
+	if serverid <= 0 {
+		err = fmt.Errorf("The serverid param should be > 0")
+		return
+	}
+
+	err = c.get(fmt.Sprintf("domains/smarthosts/%d/%d", domainid, serverid), server)
+
 	return
 }
 
 // CreateDomainSmartHost creates a domain smarthost
 // https://www.baruwa.com/docs/api/#create-a-domain-smarthost
-func (c *Client) CreateDomainSmartHost(server *DomainSmartHost) (err error) {
+func (c *Client) CreateDomainSmartHost(domainid int, server *DomainSmartHost) (err error) {
+	var v url.Values
+
+	if domainid <= 0 {
+		err = fmt.Errorf("The domainid param should be > 0")
+		return
+	}
+
+	if server == nil {
+		err = fmt.Errorf("The server param cannot be nil")
+		return
+	}
+
+	if v, err = query.Values(server); err != nil {
+		return
+	}
+
+	err = c.post(fmt.Sprintf("domains/smarthosts/%d", domainid), v, server)
+
 	return
 }
 
 // UpdateDomainSmartHost updates a domain smarthost
 // https://www.baruwa.com/docs/api/#update-a-domain-smarthost
-func (c *Client) UpdateDomainSmartHost(server *DomainSmartHost) (err error) {
+func (c *Client) UpdateDomainSmartHost(domainid int, server *DomainSmartHost) (err error) {
+	var v url.Values
+
+	if domainid <= 0 {
+		err = fmt.Errorf("The domainid param should be > 0")
+		return
+	}
+
+	if server == nil {
+		err = fmt.Errorf("The server param cannot be nil")
+		return
+	}
+
+	if server.ID <= 0 {
+		err = fmt.Errorf("The server.ID param should be > 0")
+		return
+	}
+
+	if v, err = query.Values(server); err != nil {
+		return
+	}
+
+	err = c.put(fmt.Sprintf("domains/smarthosts/%d/%d", domainid, server.ID), v, server)
+
 	return
 }
 
 // DeleteDomainSmartHost deletes a domain smarthost
 // https://www.baruwa.com/docs/api/#delete-a-domain-smarthost
-func (c *Client) DeleteDomainSmartHost(id int) (err error) {
+func (c *Client) DeleteDomainSmartHost(domainid int, server *DomainSmartHost) (err error) {
+	var v url.Values
+
+	if domainid <= 0 {
+		err = fmt.Errorf("The domainid param should be > 0")
+		return
+	}
+
+	if server == nil {
+		err = fmt.Errorf("The server param cannot be nil")
+		return
+	}
+
+	if server.ID <= 0 {
+		err = fmt.Errorf("The server.ID param should be > 0")
+		return
+	}
+
+	if v, err = query.Values(server); err != nil {
+		return
+	}
+
+	err = c.delete(fmt.Sprintf("domains/smarthosts/%d/%d", domainid, server.ID), v)
+
 	return
 }
