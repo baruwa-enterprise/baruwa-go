2874812ef9de8805e162ed60b2dfb8824d994685
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-29 13:05:05 +0200

    FET: Relay Settings implementation

--- api/organization_relays.go
+++ api/organization_relays.go
@@ -8,6 +8,13 @@
 // Package api Golang bindings for Baruwa REST API
 package api
 
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/google/go-querystring/query"
+)
+
 // RelaySetting holds relay settings
 type RelaySetting struct {
 	ID              int     `json:"id,omitempty"`
@@ -28,24 +35,85 @@ type RelaySetting struct {
 
 // GetRelaySetting returns radius settings
 // https://www.baruwa.com/docs/api/#retrieve-relay-settings
-func (c *Client) GetRelaySetting(id int) (server *RelaySetting, err error) {
+func (c *Client) GetRelaySetting(relayID int) (server *RelaySetting, err error) {
+	if relayID <= 0 {
+		err = fmt.Errorf("The relayID param should be > 0")
+		return
+	}
+
+	err = c.get(fmt.Sprintf("relays/%d", relayID), server)
+
 	return
 }
 
 // CreateRelaySetting creates radius settings
 // https://www.baruwa.com/docs/api/#create-relay-settings
-func (c *Client) CreateRelaySetting(server *RelaySetting) (err error) {
+func (c *Client) CreateRelaySetting(organizationID int, server *RelaySetting) (err error) {
+	var v url.Values
+
+	if organizationID <= 0 {
+		err = fmt.Errorf("The organizationID param should be > 0")
+		return
+	}
+
+	if server == nil {
+		err = fmt.Errorf("The server param cannot be nil")
+		return
+	}
+
+	if v, err = query.Values(server); err != nil {
+		return
+	}
+
+	err = c.post(fmt.Sprintf("relays/%d", organizationID), v, server)
+
 	return
 }
 
 // UpdateRelaySetting updates radius settings
 // https://www.baruwa.com/docs/api/#update-relay-settings
 func (c *Client) UpdateRelaySetting(server *RelaySetting) (err error) {
+	var v url.Values
+
+	if server == nil {
+		err = fmt.Errorf("The server param cannot be nil")
+		return
+	}
+
+	if server.ID <= 0 {
+		err = fmt.Errorf("The server.ID param should be > 0")
+		return
+	}
+
+	if v, err = query.Values(server); err != nil {
+		return
+	}
+
+	err = c.put(fmt.Sprintf("relays/%d", server.ID), v, server)
+
 	return
 }
 
 // DeleteRelaySetting deletes radius settings
 // https://www.baruwa.com/docs/api/#delete-relay-settings
-func (c *Client) DeleteRelaySetting(id int) (err error) {
+func (c *Client) DeleteRelaySetting(server *RelaySetting) (err error) {
+	var v url.Values
+
+	if server == nil {
+		err = fmt.Errorf("The server param cannot be nil")
+		return
+	}
+
+	if server.ID <= 0 {
+		err = fmt.Errorf("The server.ID param should be > 0")
+		return
+	}
+
+	if v, err = query.Values(server); err != nil {
+		return
+	}
+
+	err = c.delete(fmt.Sprintf("relays/%d", server.ID), v)
+
 	return
 }
