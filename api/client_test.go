e66aff06f5c0ed8fe9794e4a47dbe03c071fdb8a
Andrew Colin Kissa <andrew@topdog.za.net>
2019-02-01 13:05:36 +0200

    TST: Add client tests

--- api/client_test.go
+++ api/client_test.go
@@ -6,3 +6,69 @@
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 
 package api
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestNewErrors(t *testing.T) {
+	c, e := New("", "", nil)
+	if e == nil {
+		t.Fatalf("An error should be returned as endpoint is required")
+	}
+	if e.Error() != endpointError {
+		t.Errorf("Expected %s got %s", endpointError, e)
+	}
+	if c != nil {
+		t.Errorf("Expected %v got %v", nil, c)
+	}
+	c, e = New("http://[fe80::%31]:8080/", "", nil)
+	if e == nil {
+		t.Fatalf("An error should be returned as endpoint is required")
+	}
+}
+
+func TestNew(t *testing.T) {
+	bu := "https://baruwa.example.com"
+	c, e := New(bu, "", nil)
+	if e != nil {
+		t.Fatalf("An error should not be returned")
+	}
+	if c.BaseURL.String() != bu {
+		t.Errorf("Expected %s got %s", bu, c.BaseURL)
+	}
+}
+
+func TestNewOpts(t *testing.T) {
+	ua := "test-client"
+	client := http.DefaultClient
+	opts := &Options{
+		UserAgent:  ua,
+		HTTPClient: client,
+	}
+	bu := "https://baruwa.example.com"
+	c, e := New(bu, "", opts)
+	if e != nil {
+		t.Fatalf("An error should not be returned")
+	}
+	if c.BaseURL.String() != bu {
+		t.Errorf("Expected %s got %s", bu, c.BaseURL)
+	}
+	if c.UserAgent != ua {
+		t.Errorf("Expected %s got %s", ua, c.UserAgent)
+	}
+	if c.client != client {
+		t.Errorf("Expected %v got %v", client, c.client)
+	}
+}
+
+func TestApiPath(t *testing.T) {
+	p := "users"
+	expected := fmt.Sprintf("/api/%s/%s", APIVersion, p)
+	g := apiPath(p)
+	if g != expected {
+		t.Errorf("Expected %s got %s", expected, g)
+	}
+}
