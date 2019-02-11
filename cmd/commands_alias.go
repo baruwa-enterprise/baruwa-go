8a009d8af5e62e5468c27a9c8bb7e7003bfc1ad0
Andrew Colin Kissa <andrew@topdog.za.net>
2019-02-11 11:12:32 +0200

    FET: Alias address management cmdline implementation

--- cmd/commands_alias.go
+++ cmd/commands_alias.go
@@ -9,17 +9,177 @@
 package cmd
 
 import (
+	"fmt"
+	"log"
+
+	"github.com/baruwa-enterprise/baruwa-go/api"
+	prettyjson "github.com/hokaccha/go-prettyjson"
 	cli "github.com/jawher/mow.cli"
 )
 
 func aliasShow(cmd *cli.Cmd) {
+	var b []byte
+	var id *int
+	var err error
+	var a *api.AliasAddress
+	var c *api.Client
+
+	cmd.Spec = "-a"
+	id = cmd.Int(cli.IntOpt{
+		Name: "a alias-id",
+		Desc: "Alias address id",
+	})
+
+	cmd.Action = func() {
+		if c, err = GetClient(); err != nil {
+			log.Fatal(err)
+		}
+
+		if a, err = c.GetAliasAddress(*id); err != nil {
+			log.Fatal(err)
+		}
+
+		if b, err = prettyjson.Marshal(a); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", b)
+	}
 }
 
 func aliasCreate(cmd *cli.Cmd) {
+	var uid *int
+	var b []byte
+	var err error
+	var email *string
+	var enabled *bool
+	var c *api.Client
+	var a *api.AliasAddress
+
+	cmd.Spec = "-u --alias-address --enabled"
+	uid = cmd.Int(cli.IntOpt{
+		Name: "u uid",
+		Desc: "User ID",
+	})
+	email = cmd.String(cli.StringOpt{
+		Name: "alias-address",
+		Desc: "Alias Address",
+	})
+	enabled = cmd.Bool(cli.BoolOpt{
+		Name: "enabled",
+		Desc: "Enable or disable this alias",
+	})
+
+	cmd.Action = func() {
+		a = &api.AliasAddress{
+			Address: *email,
+			Enabled: *enabled,
+		}
+
+		if c, err = GetClient(); err != nil {
+			log.Fatal(err)
+		}
+
+		if err = c.CreateAliasAddress(*uid, a); err != nil {
+			log.Fatal(err)
+		}
+
+		if b, err = prettyjson.Marshal(a); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", b)
+	}
 }
 
 func aliasUpdate(cmd *cli.Cmd) {
+	var b []byte
+	var err error
+	var aid *int
+	var email *string
+	var enabled *bool
+	var c *api.Client
+	var a *api.AliasAddress
+	var enableSet, disableSet, emailSet bool
+
+	aid = cmd.Int(cli.IntOpt{
+		Name: "aid",
+		Desc: "Alias address ID",
+	})
+	email = cmd.String(cli.StringOpt{
+		Name:      "alias-address",
+		Desc:      "Alias Address",
+		SetByUser: &emailSet,
+	})
+	enabled = cmd.Bool(cli.BoolOpt{
+		Name:      "enable",
+		Desc:      "Enable this alias",
+		SetByUser: &enableSet,
+	})
+	enabled = cmd.Bool(cli.BoolOpt{
+		Name:      "disable",
+		Desc:      "Disable this alias",
+		SetByUser: &disableSet,
+	})
+
+	cmd.Spec = "--aid [--alias-address] --enable|--disable"
+
+	cmd.Action = func() {
+		if c, err = GetClient(); err != nil {
+			log.Fatal(err)
+		}
+
+		if a, err = c.GetAliasAddress(*aid); err != nil {
+			log.Fatal(err)
+		}
+
+		if emailSet {
+			a.Address = *email
+		}
+		if enableSet {
+			*enabled = true
+		}
+		if disableSet {
+			*enabled = false
+		}
+		a.Enabled = *enabled
+
+		if err = c.UpdateAliasAddress(a); err != nil {
+			log.Fatal(err)
+		}
+
+		if b, err = prettyjson.Marshal(a); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", b)
+		fmt.Printf("The alias address: %s has been updated\n", a.Address)
+	}
 }
 
 func aliasDelete(cmd *cli.Cmd) {
+	var err error
+	var aid *int
+	var c *api.Client
+	var a *api.AliasAddress
+
+	aid = cmd.Int(cli.IntOpt{
+		Name: "aid",
+		Desc: "Alias address ID",
+	})
+
+	cmd.Spec = "--aid"
+
+	cmd.Action = func() {
+		if c, err = GetClient(); err != nil {
+			log.Fatal(err)
+		}
+
+		if a, err = c.GetAliasAddress(*aid); err != nil {
+			log.Fatal(err)
+		}
+
+		if err = c.DeleteAliasAddress(a); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("The alias address: %s has been deleted\n", a.Address)
+	}
 }
