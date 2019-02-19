d599dbdb7bfd3d84e7a8327ef230e24e4706f82c
Andrew Colin Kissa <andrew@topdog.za.net>
2019-02-19 16:48:11 +0200

    DOC: Add example usage

--- api/doc.go
+++ api/doc.go
@@ -12,6 +12,64 @@ Usage:
 
 	import "github.com/baruwa-enterprise/baruwa-go/api"
 
+Create a new Baruwa API client, then use the various methods on the client to
+access different parts of the Baruwa REST API. For example:
+
+	import (
+		"fmt"
+		"log"
+		"os"
+
+		"github.com/baruwa-enterprise/baruwa-go/api"
+		prettyjson "github.com/hokaccha/go-prettyjson"
+	)
+
+	func main() {
+		var (
+			err                 error
+			c                   *api.Client
+			u                   *api.UserList
+			opts                *api.ListOptions
+			serverURL, apiToken string
+		)
+
+		serverURL = os.Getenv("BARUWA_API_SERVER")
+		apiToken = os.Getenv("BARUWA_API_TOKEN")
+
+		if c, err = api.New(serverURL, apiToken, nil); err != nil {
+			log.Fatal(err)
+		}
+
+		// page through users
+		for {
+			if u, err = c.GetUsers(opts); err != nil {
+				log.Fatal(err)
+			}
+
+			if len(u.Items) == 0 {
+				fmt.Println()
+				break
+			}
+
+			if b, err = prettyjson.Marshal(u); err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("%s\n", b)
+
+			if u.Links.Pages.Next == "" {
+				break
+			}
+
+			opts = &api.ListOptions{
+				Page: u.Links.Pages.Next,
+			}
+		}
+	}
+
+
+Refer to the https://github.com/baruwa-enterprise/baruwactl for a full
+application built using this api for further usage information.
 
 */
 package api
