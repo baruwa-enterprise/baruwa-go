a5e591503c7c65b7b1b25e0e4760fddbebdd58b4
Andrew Colin Kissa <andrew@topdog.za.net>
2019-02-19 14:36:05 +0200

    DOC: Add command docs

--- cmd/commands_organization_fs.go
+++ cmd/commands_organization_fs.go
@@ -5,7 +5,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// Package cmd cmdline client for the Baruwa REST API
 package cmd
 
 import (
@@ -45,7 +44,7 @@ func organizationFSShow(cmd *cli.Cmd) {
 		if b, err = prettyjson.Marshal(s); err != nil {
 			log.Fatal(err)
 		}
-		
+
 		fmt.Printf("%s\n", b)
 	}
 }
